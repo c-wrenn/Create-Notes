7e88d200cf3f8578ebd39eb79d6dcd3f6fc50b59
c-wrenn <crislynwren@yahoo.com>
2023-10-17 16:00:33 -0500

    "trying to fix H81 error"

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -11,18 +11,18 @@ function newId() {
 
 router.get("/notes", (req, res) => {
   //provides path
-  const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
+  //const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
   // Saves new notes on the request body
-  const apiNotes = JSON.parse(fs.readFileSync(dirPath, "utf8"));
+  const apiNotes = JSON.parse(fs.readFileSync(noteEntries, "utf8"));
   res.json(apiNotes);
 });
 
 router.post("/notes", (req, res) => {
   const allNotes = req.body;
-  const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
+ // const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
 
   //Will parse through the json data
-  const myNotes = JSON.parse(fs.readFileSync(dirPath, "utf8")); //do in get then read it
+  const myNotes = JSON.parse(fs.readFileSync(noteEntries, "utf8")); //do in get then read it
   console.log(allNotes);
 
   //Assigns each note an Id
@@ -32,22 +32,22 @@ router.post("/notes", (req, res) => {
   //adds to json data
   myNotes.push(allNotes);
   //writes to file
-  fs.writeFileSync(dirPath, JSON.stringify(myNotes));
+  fs.writeFileSync(noteEntries, JSON.stringify(myNotes));
   res.json(myNotes);
 });
 
 router.delete("/notes/:id", (req, res) => {
   // Will target id
   const id = req.params.id;
-  const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
+  //const dirPath = "/Users/crislyn/bootcamp/Create-Notes/db/db.json";
 
   //Will parse through the json data
-  const myNotes = JSON.parse(fs.readFileSync(dirPath, "utf8"));
+  const myNotes = JSON.parse(fs.readFileSync(noteEntries, "utf8"));
 
   //filters notes by their id
   updateNotes = myNotes.filter((note) => note.id !== id);
   //writes to file
-  fs.writeFileSync(dirPath, JSON.stringify(updateNotes));
+  fs.writeFileSync(noteEntries, JSON.stringify(updateNotes));
   //shows new array
   res.json(updateNotes);
   console.log("note deleted");
